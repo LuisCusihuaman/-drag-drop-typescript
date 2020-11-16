8aff33f5e26f370e9cc259e44812c83ba5bd2cf0
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-16 16:27:31 -0300

    chore: handle submit inputs in form

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ class ProyectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
+
 	constructor() {
 		this.templateElement = document.getElementById(
 			"project-input"
@@ -12,10 +16,28 @@ class ProyectInput {
 			true
 		);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = "user-input";
+		this.titleInputElement = this.element.querySelector(
+			"#title"
+		) as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement;
+		this.configure();
 		this.attach();
 	}
 	private attach() {
 		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+	}
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
 	}
 }
 const prjInput = new ProyectInput();
