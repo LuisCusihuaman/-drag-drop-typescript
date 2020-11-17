49448693de8dd1cbbb66873448533fe55dd7c29e
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-17 17:26:16 -0300

    feat: drag and drop transfer change the item state

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,16 @@ class ProjectState extends State<Project> {
 			ProjectStatus.Active
 		);
 		this.projects.push(newProject);
+		this.updateListeners();
+	}
+	moveProject(projectId: string, newStatus: ProjectStatus) {
+		const project = this.projects.find((p) => p.id === projectId);
+		if (project && project.status !== newStatus) {
+			project.status = newStatus;
+			this.updateListeners();
+		}
+	}
+	private updateListeners() {
 		for (const listenerFn of this.listeners) {
 			listenerFn(this.projects.slice());
 		}
@@ -176,7 +186,8 @@ class ProjectItem
 	}
 	@autobind
 	dragStartHandler(event: DragEvent): void {
-		console.log(event);
+		event.dataTransfer!.setData("text/plain", this.project.id);
+		event.dataTransfer!.effectAllowed = "move";
 	}
 	dragEndHandler(_: DragEvent): void {
 		console.log("DragEnd");
@@ -205,17 +216,30 @@ class ProjectList
 		this.renderContent();
 	}
 	@autobind
-	dragOverHandler(_: DragEvent): void {
-		const listEl = this.element.querySelector("ul")!;
-		listEl.classList.add("droppable");
+	dragOverHandler(event: DragEvent): void {
+		if (
+			event.dataTransfer &&
+			event.dataTransfer.types[0] === "text/plain"
+		) {
+			event.preventDefault();
+			const listEl = this.element.querySelector("ul")!;
+			listEl.classList.add("droppable");
+		}
 	}
 	@autobind
 	dragLeaveHandler(_: DragEvent): void {
 		const listEl = this.element.querySelector("ul")!;
 		listEl.classList.remove("droppable");
 	}
-	dropHandler(_: DragEvent): void {
-		throw new Error("Method not implemented.");
+	@autobind
+	dropHandler(event: DragEvent): void {
+		const prjId = event.dataTransfer!.getData("text/plain");
+		projectState.moveProject(
+			prjId,
+			this.type === "active"
+				? ProjectStatus.Active
+				: ProjectStatus.Finished
+		);
 	}
 	configure() {
 		this.element.addEventListener("dragover", this.dragOverHandler);
