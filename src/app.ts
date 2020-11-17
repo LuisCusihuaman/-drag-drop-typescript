ae3f253ca2427fae4df0c67ba1ef3b4de8762495
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-17 09:42:09 -0300

    improvement: rich typed aggregate

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
-// Project State Managment Class
+const enum ProjectStatus {
+	Active,
+	Finished,
+}
 
+//Project
+class Project {
+	constructor(
+		public id: string,
+		public title: string,
+		public description: string,
+		public people: number,
+		public status: ProjectStatus
+	) {}
+}
+
+type Listener = (items: Project[]) => void;
+
+// Project State Managment Class
 class ProjectState {
-	private listeners: any[] = [];
-	private projects: any[] = [];
+	private listeners: Listener[] = [];
+	private projects: Project[] = [];
 	private static instance: ProjectState;
 	private constructor() {}
 	static getInstance() {
@@ -12,16 +29,17 @@ class ProjectState {
 		this.instance = new ProjectState();
 		return this.instance;
 	}
-	addListener(listenerFn: Function) {
+	addListener(listenerFn: Listener) {
 		this.listeners.push(listenerFn);
 	}
 	addProject(title: string, description: string, numOfPeople: number) {
-		const newProject = {
-			id: Math.random().toString(),
-			title: title,
-			description: description,
-			people: numOfPeople,
-		};
+		const newProject = new Project(
+			Math.random().toString(),
+			title,
+			description,
+			numOfPeople,
+			ProjectStatus.Active
+		);
 		this.projects.push(newProject);
 		for (const listenerFn of this.listeners) {
 			listenerFn(this.projects.slice());
@@ -93,7 +111,7 @@ class ProjectList {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLElement;
-	assignedProjects: any[];
+	assignedProjects: Project[];
 	constructor(private type: "active" | "finished") {
 		this.templateElement = document.getElementById(
 			"project-list"
@@ -106,7 +124,7 @@ class ProjectList {
 		);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
 		this.element.id = `${this.type}-projects`;
-		projectState.addListener((projects: any[]) => {
+		projectState.addListener((projects: Project[]) => {
 			this.assignedProjects = projects;
 			this.renderProjects();
 		});
