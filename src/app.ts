6a69620f8283921c80812d83da455c521209d317
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-17 10:04:07 -0300

    fix: filter projects by state

--- src/app.ts
+++ src/app.ts
@@ -125,7 +125,12 @@ class ProjectList {
 		this.element = importedNode.firstElementChild as HTMLFormElement;
 		this.element.id = `${this.type}-projects`;
 		projectState.addListener((projects: Project[]) => {
-			this.assignedProjects = projects;
+			const relevantProjects = projects.filter((prj: Project) => {
+				return this.type === "active"
+					? prj.status == ProjectStatus.Active
+					: prj.status === ProjectStatus.Finished;
+			});
+			this.assignedProjects = relevantProjects;
 			this.renderProjects();
 		});
 
@@ -136,6 +141,7 @@ class ProjectList {
 		const listEl = document.getElementById(
 			`${this.type}-projects-list`
 		)! as HTMLUListElement;
+		listEl.innerHTML = "";
 		for (const prjItem of this.assignedProjects) {
 			const listItem = document.createElement("li");
 			listItem.textContent = prjItem.title;
