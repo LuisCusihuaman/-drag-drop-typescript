9025d83d6efa922d926faaebf589dad99a8b6a96
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-18 20:58:55 -0300

    feat(Todo): working with State and Types

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewTodo from './component/NewTodo';
+import { Todo } from './component/todo.model';
 import TodoList from './component/TodoList';
 
 export const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos((prevTodos) => [...prevTodos, { id: Math.random().toString(), text }]);
   };
   return (
     <div className="App">
