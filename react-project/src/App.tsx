293fe5d4c9ed428cbe75af4d572bdb16a64d9031
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-18 20:07:09 -0300

    feat: getting user-input with refs

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import NewTodo from './component/NewTodo';
 import TodoList from './component/TodoList';
 
 export const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'Finish the course' }];
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
