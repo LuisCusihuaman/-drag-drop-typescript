053a4ffd14c6567a043796e297a03cceb8b39d8d
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-11-17 23:59:57 -0300

    style: separated files into folders

---
+++ src/models/project.ts
@@ -0,0 +1,15 @@
+export const enum ProjectStatus {
+	Active,
+	Finished,
+}
+
+//Project
+export class Project {
+	constructor(
+		public id: string,
+		public title: string,
+		public description: string,
+		public people: number,
+		public status: ProjectStatus
+	) {}
+}
